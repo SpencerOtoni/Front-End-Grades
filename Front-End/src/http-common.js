5aa1859fd770cec2af0d8a115b5630570d29f941
Spencer <sspencerotoni@gmail.com>
2020-09-05 18:50:01 -0300

    Correção baseURL

--- Front-End/src/http-common.js
+++ Front-End/src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://back-end-grades.herokuapp.com',
+  baseURL: 'https://back-end-grades.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
